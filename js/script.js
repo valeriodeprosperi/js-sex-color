344e9dcc5f96f94e64c22300bdfd20e7c2b56e62
valerio <valerio.deprosperi@gmail.com>
2020-08-06 19:54:17 +0200

    genereUtente not defined da risolvere

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
 // chiedere all'utente nome
 var nomeUtente = prompt("inserisci il tuo nome");
 // chiedere all'utente il genere
-var genereUtene = prompt("sei maschio o femmina?");
+var genereUtene = prompt("inserisci il tuo genere (maschio o femmina)");
 // seleziono nome nell'html
 var nome = document.getElementById('nome_utente');
 // se maschio colore blu
-if (genere == "maschio"){
-nome.className = nome.classList + "nomeBlu";
+if (genereUtente == "maschio") {
+document.getElementById("nome_utente").innerHTML = nomeUtente;
+nome.style.color = "blue";
 }
