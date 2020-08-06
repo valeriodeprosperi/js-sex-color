c050a9c390558a39ae64792acebb56cf1dfe084b
valerio <valerio.deprosperi@gmail.com>
2020-08-06 18:34:21 +0200

    if genere maschio e css color blu e rosa

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// chiedere all'utente nome
+var nomeUtente = prompt("inserisci il tuo nome");
+// chiedere all'utente il genere
+var genereUtene = prompt("sei maschio o femmina?");
+// seleziono nome nell'html
+var nome = document.getElementById('nome_utente');
+// se maschio colore blu
+if (genere == "maschio"){
+
+}
