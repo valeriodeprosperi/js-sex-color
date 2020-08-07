51704817b08a8823cdbcd22931a43a3d4510dfa3
valerio <valerio.deprosperi@gmail.com>
2020-08-07 09:14:31 +0200

    problema risolto,esercizio completato.

--- js/script.js
+++ js/script.js
@@ -1,11 +1,16 @@
 // chiedere all'utente nome
 var nomeUtente = prompt("inserisci il tuo nome");
 // chiedere all'utente il genere
-var genereUtene = prompt("inserisci il tuo genere (maschio o femmina)");
+var genereUtente = prompt("inserisci il tuo genere (maschio o femmina)");
 // seleziono nome nell'html
 var nome = document.getElementById('nome_utente');
 // se maschio colore blu
 if (genereUtente == "maschio") {
 document.getElementById("nome_utente").innerHTML = nomeUtente;
 nome.style.color = "blue";
+} else if (genereUtente == "femmina"){
+  document.getElementById("nome_utente").innerHTML = nomeUtente;
+  nome.style.color = "pink";
+} else {
+  alert ("errore,inserisci dati correttamente!")
 }
